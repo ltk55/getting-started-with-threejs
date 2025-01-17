38db9d27d9bfa5e5b18bc59560e448a846e7c84e
Frank Lam <franlam55@gmail.com>
2025-01-17 11:04:13 +0000

    Add OrbitControls and enhance scene with lighting and geometry

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-const renderer = new THREE.WebGLRenderer({ antialias: true }); // Create a renderer
-renderer.setSize(w, h); // Set size of renderer
-document.body.appendChild(renderer.domElement); // Add renderer to DOM
-
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
 const fov = 75;
 const aspect = w / h;
 const near = 0.1;
 const far = 10;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); // Create a camera
-camera.position.z = 2; // Set camera position
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+const scene = new THREE.Scene();
 
-const scene = new THREE.Scene(); // Create a scene
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
 
-const geo = new THREE.IcosahedronGeometry(1, 2); // Create a geometry
-const mat = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Create a material
-const mesh = new THREE.Mesh(geo, mat); // Create a mesh
-scene.add(mesh); // Add mesh to scene
+const geo = new THREE.IcosahedronGeometry(1.0, 1.0);
+const mat = new THREE.MeshStandardMaterial({
+  color: 0x0066ff,
+  flatShading: true,
+  transparent: true,
+  opacity: 1.0,
+});
+const wireMat = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001);
+const mesh = new THREE.Mesh(geo, mat);
+mesh.add(wireMesh);
+scene.add(mesh);
 
-renderer.render(scene, camera); // Render scene
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0xff0000);
+scene.add(hemiLight);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  controls.update();
+}
+animate();
